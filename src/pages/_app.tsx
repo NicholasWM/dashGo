ed6778f2b801c40efd37bcbb589e939a7f739945
NicholasWM <nicholaswesleymazzei@gmail.com>
2021-10-11 23:35:48 -0300

    Set up React-Query and axios

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,23 @@ import {AppProps} from 'next/app'
 import {ChakraProvider} from '@chakra-ui/react'
 import { theme } from '../styles/theme'
 import { SideBarDrawerProvider } from '../contexts/SiderBarDrawerContext'
+import { makeServer } from '../services/mirage'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+if(process.env.NODE_ENV === "development"){
+  makeServer()
+}
+const client = new QueryClient()
 function MyApp({ Component, pageProps }:AppProps) {
   return (
-    <ChakraProvider theme={theme} >
-      <SideBarDrawerProvider>
-        <Component {...pageProps} />
-      </SideBarDrawerProvider>
-    </ChakraProvider>
+    <QueryClientProvider client={client}>
+      <ChakraProvider theme={theme} >
+        <SideBarDrawerProvider>
+          <Component {...pageProps} />
+        </SideBarDrawerProvider>
+      </ChakraProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   )
 }
 
